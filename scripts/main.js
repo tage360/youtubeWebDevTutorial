00b245982e9d0b6d76916a841f8bff0955159419
Anthony <tage360@gmail.com>
2018-08-12 11:20:48 -0700

    end of java 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-alert();
+// pice of code that dose one or more actions
+
+function go(name, age){
+	if (age < 20){
+		return name + '!';
+	} else {
+		return name;
+	}
+}
+
+function add(first, second){
+	return first + second;
+}
+
+alert(go('will', 19));
+//alert(add(2,3));
